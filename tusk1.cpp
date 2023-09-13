1b825d8310d275db9f267f3197bed6fb787dc63e
Sergey Potapov <serfserf15@gmail.com>
2023-09-13 19:11:58 +0400

    initial

---
+++ tusk1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+int main() 
+{
+	std::ofstream file("tut.txt", std::ios::app );
+	std::string name, name1, date;
+	int deposit;
+	std::cin >> name >> name1 >> date >> deposit;
+	file << name << " " << name1 << " " << date << " " << deposit << std::endl;
+	file.close();
+}
